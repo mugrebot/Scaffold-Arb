6868af2543b602d962dede09f1485f3486c0e441
Mugrebot <mikeongay@icloud.com>
2022-08-18 15:53:37 -0400

    big numbers right

--- packages/react-app/src/components/BSC_price.tsx
+++ packages/react-app/src/components/BSC_price.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { Address } from 'cluster';
 import { BSC_API } from "../constants";
 
-import { ethers } from "ethers";
+import { ethers, BigNumber } from "ethers";
 
 const lock = new AsyncLock();
 
@@ -26,6 +26,10 @@ const addresses = {
 }
 
 const poolImmutablesAbi = [
+    "function factory() external view returns (address)",
+    "function token0() external view returns (address)",
+    "function token1() external view returns (address)",
+    "function MINIMUM_LIQUIDITY() external pure returns (uint)",
   "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)",
 ];
 
@@ -40,23 +44,61 @@ const bscProvider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed1.
 const poolContract = new ethers.Contract(poolAddress, poolImmutablesAbi, bscProvider);
 
 interface Immutables {
-  getReserves: number
+    factory: string
+    token0: string
+    token1: string
+  getReserves: Array<any>
 }
 
+interface State {
+    liquidity: ethers.BigNumber
+  }
+
 async function getPoolImmutables() {
-    const [getReserves] = await Promise.all([
-      poolContract.getReserves(),
+    const [factory, token0, token1, getReserves] = await Promise.all([
+        poolContract.factory(),
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.getReserves(),
     ])
 
     const immutables: Immutables = {
+        factory,
+        token0,
+        token1,
         getReserves,
       }
       return immutables
     }
 
+    async function getPoolState() {
+        const [liquidity] = await Promise.all([poolContract.MINIMUM_LIQUIDITY()])
+      
+        const PoolState: State = {
+          liquidity,
+
+        }
+      
+        return PoolState
+      }
+      
 getPoolImmutables().then(result => {
   console.log(result);
+    const token0ammt = ethers.utils.formatEther(result.getReserves[0])
+    const token1ammt = ethers.utils.formatEther(result.getReserves[1])
+    const both = Number(token0ammt)/Number(token1ammt)
+    console.log(token0ammt, token1ammt, both)
+
+    
+
 });
+
+getPoolState().then(result2 => {
+    console.log(result2.liquidity, ethers.utils.formatEther(result2.liquidity));
+    
+});
+
+
 
 
 export async function getBnbPrice(): Promise<number> {
@@ -74,3 +116,5 @@ export async function getBnbPrice(): Promise<number> {
     return bnbPrice;
   });
 }
+
+
